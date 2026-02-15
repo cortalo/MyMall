fefaef32e737d1c3aba15ee42ac52b92fba33a27
Cortalo <Cortalo@outlook.com>
2026-02-15 17:22:33 +0100

    init

---
+++ src/test/java/com/longhe/learn/mymall/MyMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longhe.learn.mymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
